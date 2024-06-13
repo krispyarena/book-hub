dff699b34dcde82c0a3f232fd53958eeeaadb0e1
krispyarena <krispy.arena@gmail.com>
2024-06-13 17:01:23 +0545

    Added a new controller with its view

---
+++ BookHub/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookHub.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
