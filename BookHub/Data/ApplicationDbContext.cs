1d168934470d71f67548f2e441292422352f64be
krispyarena <krispy.arena@gmail.com>
2024-06-13 16:34:14 +0545

    Categories table is created using EFCore

---
+++ BookHub/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookHub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookHub.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- BookHub/Program.cs
+++ BookHub/Program.cs
@@ -1,7 +1,13 @@
+using BookHub.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
