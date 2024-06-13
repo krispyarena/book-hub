040b8287761e1ba1e3043fd51001802db1a78818
krispyarena <krispy.arena@gmail.com>
2024-06-13 15:50:00 +0545

    Nuget Packages are installed and Category model is added

---
+++ BookHub/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace BookHub.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
