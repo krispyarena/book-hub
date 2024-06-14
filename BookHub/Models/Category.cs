a4eb38eec9547d83762a58718a246693b11cf38e
krispyarena <krispy.arena@gmail.com>
2024-06-14 15:49:16 +0545

    Input tag helpers are used

--- BookHub/Models/Category.cs
+++ BookHub/Models/Category.cs
@@ -1,9 +1,13 @@
-﻿namespace BookHub.Models
+﻿using System.ComponentModel;
+
+namespace BookHub.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
