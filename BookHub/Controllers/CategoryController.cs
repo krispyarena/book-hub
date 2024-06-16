cd3c73862427e2cbe624ac15477d7464cd0bdcb9
krispyarena <krispy.arena@gmail.com>
2024-06-16 13:23:24 +0545

    code for editing a category is completed

--- BookHub/Controllers/CategoryController.cs
+++ BookHub/Controllers/CategoryController.cs
@@ -12,7 +12,7 @@ namespace BookHub.Controllers
         {
             _db = db;
         }
-            
+
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
@@ -35,9 +35,42 @@ namespace BookHub.Controllers
             {
                 _db.Categories.Add(category);
                 _db.SaveChanges();
-				return RedirectToAction("Index");
-			}
+                return RedirectToAction("Index");
+            }
             return View();
-		}
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id == id);    
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id == id).FirstOrDefault();    
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
