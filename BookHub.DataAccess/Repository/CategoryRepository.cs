4dcd6b8407d4e76f2466f406e94267a331c363b6
krispyarena <krispy.arena@gmail.com>
2024-06-18 16:18:01 +0545

    ICategoryRepository has been implemented

---
+++ BookHub.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BookHub.DataAccess.Data;
+using BookHub.DataAccess.Repository.IRepository;
+using BookHub.Models;
+
+namespace BookHub.DataAccess.Repository
+{
+	public class CategoryRepository : Repository<Category>, ICategoryRepository
+	{
+		private ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+		{
+			_db.SaveChanges();
+		}
+
+		public void Update(Category obj)
+		{
+			_db.Categories.Update(obj);
+		}
+	}
+}
---
+++ BookHub.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookHub.Models;
+
+namespace BookHub.DataAccess.Repository.IRepository
+{
+	public interface ICategoryRepository : IRepository<Category>
+	{
+		void Update(Category obj);
+
+		void Save();
+	}
+}
