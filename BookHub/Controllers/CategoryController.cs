04214f40a96aeaef37d98ed3068d311316470f12
krispyarena <krispy.arena@gmail.com>
2024-06-18 16:33:55 +0545

    Service added to dependency injection

--- BookHub/Controllers/CategoryController.cs
+++ BookHub/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BookHub.DataAccess.Data;
+using BookHub.DataAccess.Repository.IRepository;
 using BookHub.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,16 +7,16 @@ namespace BookHub.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _db;
+        private readonly ICategoryRepository _categoryRepo;
 
-        public CategoryController(ApplicationDbContext db)
+        public CategoryController(ICategoryRepository db)
         {
-            _db = db;
+            _categoryRepo = db;
         }
 
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _db.Categories.ToList();
+            List<Category> objCategoryList = _categoryRepo.GetAll().ToList();
             return View(objCategoryList);
         }
 
@@ -33,8 +34,8 @@ namespace BookHub.Controllers
             }
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(category);
-                _db.SaveChanges();
+                _categoryRepo.Add(category);
+                _categoryRepo.Save();
                 TempData["success"] = "Category Created Successfully";
                 return RedirectToAction("Index");
             }
@@ -47,7 +48,7 @@ namespace BookHub.Controllers
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _db.Categories.Find(id);
+            Category? categoryFromDb = _categoryRepo.Get(u=>u.Id==id);
             //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id == id);    
             //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id == id).FirstOrDefault();    
             if (categoryFromDb == null)
@@ -66,8 +67,8 @@ namespace BookHub.Controllers
             }
             if (ModelState.IsValid)
             {
-                _db.Categories.Update(obj);
-                _db.SaveChanges();
+                _categoryRepo.Update(obj);
+                _categoryRepo.Save();
 
 				TempData["success"] = "Category Updated Successfully";
 
@@ -82,7 +83,7 @@ namespace BookHub.Controllers
 			{
 				return NotFound();
 			}
-			Category? categoryFromDb = _db.Categories.Find(id);
+			Category? categoryFromDb = _categoryRepo.Get(u=>u.Id==id);
 			//Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id == id);    
 			//Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id == id).FirstOrDefault();    
 			if (categoryFromDb == null)
@@ -96,13 +97,13 @@ namespace BookHub.Controllers
 
 		public IActionResult DeletePost(int? id)
 		{
-            Category? obj = _db.Categories.Find(id);
+            Category? obj = _categoryRepo.Get(u => u.Id == id);
             if (obj == null)
             {
                 return NotFound();
             }
-            _db.Categories.Remove(obj);
-			_db.SaveChanges();
+            _categoryRepo.Remove(obj);
+			_categoryRepo.Save();
 
 			TempData["success"] = "Category Deleted Successfully";
 
--- BookHub/Program.cs
+++ BookHub/Program.cs
@@ -1,4 +1,6 @@
 using BookHub.DataAccess.Data;
+using BookHub.DataAccess.Repository;
+using BookHub.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>(); 
 
 var app = builder.Build();
 
