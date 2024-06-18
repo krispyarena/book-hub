2d441ec0f359f99c3b199e8420facef0d780f595
krispyarena <krispy.arena@gmail.com>
2024-06-18 15:44:38 +0545

    Interface is created for repository pattern

---
+++ BookHub/Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace BookHub.Repository.IRepository
+{
+	public interface IRepository<T> where T : class
+	{
+		//T - Category
+		IEnumerable<T> GetAll();
+		T Get(Expression<Func<T, bool>> filter);
+
+		void Add(T entity);
+		void Remove(T entity);
+		void RemoveRange(IEnumerable<T> entity);
+
+	}
+}
