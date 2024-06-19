a60fcb225523b59a47103f7ddd1a49fb1cb9c016
krispyarena <krispy.arena@gmail.com>
2024-06-19 19:14:34 +0545

    Foreign Key added in EF Core

--- BookHub.DataAccess/Data/ApplicationDbContext.cs
+++ BookHub.DataAccess/Data/ApplicationDbContext.cs
@@ -34,7 +34,8 @@ namespace BookHub.DataAccess.Data
                     ListPrice = 99,
                     Price = 90,
                     Price50 = 85,
-                    Price100 = 80
+                    Price100 = 80,
+                    CategoryId = 1,
                 },
                 new Product
                 {
@@ -46,7 +47,8 @@ namespace BookHub.DataAccess.Data
                     ListPrice = 40,
                     Price = 30,
                     Price50 = 25,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 2,
                 },
                 new Product
                 {
@@ -58,7 +60,8 @@ namespace BookHub.DataAccess.Data
                     ListPrice = 55,
                     Price = 50,
                     Price50 = 40,
-                    Price100 = 35
+                    Price100 = 35,
+                    CategoryId = 3,
                 },
                 new Product
                 {
@@ -70,7 +73,8 @@ namespace BookHub.DataAccess.Data
                     ListPrice = 70,
                     Price = 65,
                     Price50 = 60,
-                    Price100 = 55
+                    Price100 = 55,
+                    CategoryId = 3,
                 },
                 new Product
                 {
@@ -82,7 +86,8 @@ namespace BookHub.DataAccess.Data
                     ListPrice = 30,
                     Price = 27,
                     Price50 = 25,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 3,
                 },
                 new Product
                 {
@@ -94,7 +99,8 @@ namespace BookHub.DataAccess.Data
                     ListPrice = 25,
                     Price = 23,
                     Price50 = 22,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 3,
                 }
                 );
         }
--- BookHub.Models/Product.cs
+++ BookHub.Models/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,5 +39,9 @@ namespace BookHub.Models
         [DisplayName("Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
     }
 }
