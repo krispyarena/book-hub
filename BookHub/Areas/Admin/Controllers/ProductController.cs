a6367b627fed2718940829f05fc0851d4b594bcd
krispyarena <krispy.arena@gmail.com>
2024-06-21 17:41:19 +0545

    Category is populated using include properties

--- BookHub/Areas/Admin/Controllers/ProductController.cs
+++ BookHub/Areas/Admin/Controllers/ProductController.cs
@@ -23,7 +23,7 @@ namespace BookHub.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
-            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
             return View(objProductList);
         }
 
