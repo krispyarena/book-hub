a36c500ecec5e90fb47b9df3a7dc3bbfe6db0075
krispyarena <krispy.arena@gmail.com>
2024-06-22 06:32:23 +0545

    Integrated DataTables third-party library for enhanced product list display

--- BookHub/Areas/Admin/Controllers/ProductController.cs
+++ BookHub/Areas/Admin/Controllers/ProductController.cs
@@ -145,5 +145,17 @@ namespace BookHub.Areas.Admin.Controllers
 
             return RedirectToAction("Index");
         }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            
+            return Json(new {data = objProductList});
+        }
+
+
+        #endregion
     }
 }
---
+++ BookHub/wwwroot/js/product.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/admin/product/getall",
+            "dataSrc": "data" // Adjust according to your API response structure
+        },
+        "columns": [
+            { "data": "title", "width": "25%" },
+            { "data": "author", "width": "15%" },
+            { "data": "isbn", "width": "15%" },
+            { "data": "listPrice", "width": "15%" },
+            { "data": "category.name", "width": "15%" }
+        ]
+    });
+}
