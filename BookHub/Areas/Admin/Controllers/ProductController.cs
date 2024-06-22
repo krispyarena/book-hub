a14b5b4be447ee5afd037072819e1eb6e6c28fe2
krispyarena <krispy.arena@gmail.com>
2024-06-22 08:19:56 +0545

    sweetalert is used for enhanced view

--- BookHub/Areas/Admin/Controllers/ProductController.cs
+++ BookHub/Areas/Admin/Controllers/ProductController.cs
@@ -113,22 +113,6 @@ namespace BookHub.Areas.Admin.Controllers
             return View(productVM);
         }
 
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id == id);    
-            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id == id).FirstOrDefault();    
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-
         [HttpPost, ActionName("Delete")]
 
         public IActionResult DeletePost(int? id)
@@ -153,6 +137,31 @@ namespace BookHub.Areas.Admin.Controllers
             List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
             
             return Json(new {data = objProductList});
+        }
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var productToBeDeleted = _unitOfWork.Product.Get(u=>u.Id == id);
+
+            if(productToBeDeleted == null)
+            {
+                return Json(new {success= false, message="Error while deleting"});
+            }
+
+            var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, 
+                productToBeDeleted.ImageUrl.TrimStart('\\'));
+
+
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            _unitOfWork.Product.Remove(productToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Sucessful" });
+
         }
 
 
