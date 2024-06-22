a988b4635ddfee32ad58e629bc9309e34c1f99ce
krispyarena <krispy.arena@gmail.com>
2024-06-22 09:01:23 +0545

    home page designed properly

--- BookHub/Areas/Customer/Controllers/HomeController.cs
+++ BookHub/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using BookHub.DataAccess.Repository.IRepository;
 using BookHub.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +9,18 @@ namespace BookHub.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
