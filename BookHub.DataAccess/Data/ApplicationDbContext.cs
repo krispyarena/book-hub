52dbb1dd9eec2ef60fbc221dab7149b192ed386f
krispyarena <krispy.arena@gmail.com>
2024-06-22 19:07:41 +0545

    shopping-cart model added

--- BookHub.DataAccess/Data/ApplicationDbContext.cs
+++ BookHub.DataAccess/Data/ApplicationDbContext.cs
@@ -16,6 +16,7 @@ namespace BookHub.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
