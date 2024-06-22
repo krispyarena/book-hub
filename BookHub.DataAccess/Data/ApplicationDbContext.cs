683d86850f2ff9be83aab6e383a103b71aba6fd0
krispyarena <krispy.arena@gmail.com>
2024-06-22 13:12:21 +0545

    Default IdentityUser extended and more columns are added in table

--- BookHub.DataAccess/Data/ApplicationDbContext.cs
+++ BookHub.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace BookHub.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ BookHub.Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookHub.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name {  get; set; }
+
+        public string? StreetAddress { get; set; }
+        public string? City {  get; set; }
+        public string? State { get; set; }
+        public string? PostalCode {  get; set; }
+    }
+}
