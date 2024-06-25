e8f8367b674d0251eaea9763c4bdc6f0f140ed68
krispyarena <krispy.arena@gmail.com>
2024-06-25 23:58:19 +0545

    OrderVM and OrderController created

---
+++ BookHub/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using BookHub.DataAccess.Repository.IRepository;
+using BookHub.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookHub.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+			return Json(new { data = objOrderHeaders });
+		}
+
+
+		#endregion
+	}
+}
