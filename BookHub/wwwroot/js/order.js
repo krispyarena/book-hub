c82c693229a9949bf80a99ac039632f0b5f418fc
krispyarena <krispy.arena@gmail.com>
2024-06-26 03:01:00 +0545

    simplified conditional statement

--- BookHub/wwwroot/js/order.js
+++ BookHub/wwwroot/js/order.js
@@ -2,26 +2,21 @@
 
 $(document).ready(function () {
     var url = window.location.search;
+
     if (url.includes("inprocess")) {
         loadDataTable("inprocess");
     }
+    else if (url.includes("completed")) {
+        loadDataTable("completed");
+    }
+    else if (url.includes("pending")) {
+        loadDataTable("pending");
+    }
+    else if (url.includes("approved")) {
+        loadDataTable("approved");
+    }
     else {
-        if (url.includes("completed")) {
-            loadDataTable("completed");
-        }
-        else {
-            if (url.includes("pending")) {
-                loadDataTable("pending");
-            }
-            else {
-                if (url.includes("approved")) {
-                    loadDataTable("approved");
-                }
-                else {
-                    loadDataTable("all");
-                }
-            }
-        }
+        loadDataTable("all");
     }
 });
 
